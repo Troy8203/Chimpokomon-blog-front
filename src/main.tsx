ef919b94902542134c5b4eb746b763d24804efc6
Roy Ayala <troysg8203@gmail.com>
2024-08-17 20:13:47 -0400

    deploy config

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import Layoud from "./layoud.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Nav />
-    {/* <Blog /> */}
+    <Blog />
 
     <Layoud>
       <FormRegister />
