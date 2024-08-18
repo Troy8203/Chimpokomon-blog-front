b561e78fd151a58fa17950cda6bca246ea746460
Roy Ayala <troysg8203@gmail.com>
2024-08-17 20:17:45 -0400

    fix: blog fixed

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,8 +1,4 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { Badge } from "@/components/ui/badge";
-import { Input } from "@/components/ui/input";
-
-import { Search } from "lucide-react";
 
 import Card from "@/components/CardBlog";
 
